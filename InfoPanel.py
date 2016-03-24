4e970be8e59292bc3b28af74eb276e9e1f0bf9d6
g-raphsMTA <jake_pinette@hotmail.com>
2016-03-24 17:18:17 -0300

    Added files via upload

--- InfoPanel.py
+++ InfoPanel.py
@@ -6,41 +6,39 @@ from Edge import Edge
 from Vertex import Vertex
 from kivy.lang import Builder
 
-from kivy.event import EventDispatcher
-
 Builder.load_file('InfoPanel.kv')
-
 
 class InfoPanel(Widget):
 
-    vertexTextInfo = StringProperty( '' )
-    vertexOutgoingEdges = StringProperty( '' )
-    vertexIncomingEdges = StringProperty( '' )
+    vertexTextInfo = StringProperty()
+    vertexOutgoingEdges = StringProperty()
+    vertexIncomingEdges = StringProperty()
 
 
 
-    def __init__(self): #*args, **kwargs
-        super(InfoPanel, self).__init__() #*args, **kwargs
+    def __init__(self, **kwargs):
+        
+        super(InfoPanel, self).__init__(**kwargs)
+        
         self.vertexTextInfo = 'This is Node info'
         self.vertexOutgoingEdges = 'This is outgoing Edge List'
         self.vertexIncomingEdges = 'This is the Incoming Edge List'
-        
-        self.bind(vertexTextInfo=self.set_vertexTextInfo)
-        self.bind(vertexOutgoingEdges=self.set_vertexOutgoingEdges)
-        self.bind(vertexIncomingEdges=self.set_vertexIncomingEdges)
 
-    def set_vertexTextInfo(self, instance, value):
-        self.vertexTextInfo = 'Test Complete, it works!' #this is where getinfo()
+    def set_vertexTextInfo(self):
+        self.vertexTextInfo = "Test Complete, it works!"
+        #this is where getinfo()
 
-    def set_vertexOutgoingEdges(self, instance, value):
-        self.vertexOutgoingEdges = 'TESTYTEST' #this is where getOutInfo()
+    def set_vertexOutgoingEdges(self):
+        self.vertexOutgoingEdges = "TESTYTEST"
+        #this is where getOutInfo()
 
-    def set_vertexIncomingEdges(self, instance, value):
-        self.vertexIncomingEdges = 'TESTYTEST TEEEST' #this is where getInInfo()
+    def set_vertexIncomingEdges(self):
+        self.vertexIncomingEdges = "TESTYTEST TEEEST"
+        #this is where getInInfo()
 
 class buildInfoPanel(App):
     def build(self):
-        panel = InfoPanel()
-        self.add_widget(panel)
         return InfoPanel()
-    
+
+if __name__ == '__main__':
+    buildInfoPanel().run()
