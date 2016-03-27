eb7d8373921413811700181f433b45db79eabaa3
g-raphsMTA <jake_pinette@hotmail.com>
2016-03-27 20:46:13 -0300

    Added files via upload

--- InfoPanel.py
+++ InfoPanel.py
@@ -5,11 +5,13 @@ from kivy.properties import StringProperty
 from Edge import Edge
 from Vertex import Vertex
 from kivy.lang import Builder
+from kivy.uix.textinput import TextInput
 
 Builder.load_file('InfoPanel.kv')
 
 class InfoPanel(Widget):
 
+    vertexName = StringProperty()
     vertexTextInfo = StringProperty()
     vertexOutgoingEdges = StringProperty()
     vertexIncomingEdges = StringProperty()
@@ -19,26 +21,35 @@ class InfoPanel(Widget):
     def __init__(self, **kwargs):
         
         super(InfoPanel, self).__init__(**kwargs)
-        
+        self.vertexName = 'this is Node Name' #this is where getInfo(for name)
         self.vertexTextInfo = 'This is Node info'
         self.vertexOutgoingEdges = 'This is outgoing Edge List'
         self.vertexIncomingEdges = 'This is the Incoming Edge List'
 
+
+    def set_vertexName(self):
+        self.vertexName = "Name set"
+        
+        
     def set_vertexTextInfo(self):
-        self.vertexTextInfo = "Test Complete, it works!"
-        #this is where getinfo()
+        self.vertexTextInfo = "Text Info Set"
+        #this is where getinfo() for text info
 
     def set_vertexOutgoingEdges(self):
-        self.vertexOutgoingEdges = "TESTYTEST"
-        #this is where getOutInfo()
-
+        self.vertexOutgoingEdges = "Outgoing Edges Set"
+        #this is where getOutInfo() for outEdges
+        
     def set_vertexIncomingEdges(self):
-        self.vertexIncomingEdges = "TESTYTEST TEEEST"
-        #this is where getInInfo()
+        self.vertexIncomingEdges = "Incoming Edges Set"
+        #this is where getInfo() for InEdges
+
+#----------------------------------------------------------------------------
 
 class buildInfoPanel(App):
     def build(self):
         return InfoPanel()
+
+#-----------------------------------------------------------------------------
 
 if __name__ == '__main__':
     buildInfoPanel().run()
