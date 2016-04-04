c32a39398f3428a5bc8352917f95eed71be63a77
g-raphsMTA <jake_pinette@hotmail.com>
2016-04-04 16:47:03 -0300

    Added files via upload

--- BellmanFord.py
+++ BellmanFord.py
@@ -1,11 +1,10 @@
 from GraphPanel import GraphPanel
 from Edge import Edge
 from Vertex import Vertex
-from Queue import Queue
 
 class BellmanFord():
     
-    def findShortestPath(Graph, startNodeIndex, endNodeIndex):
+    def findShortestPath(self, Graph, startNodeIndex, endNodeIndex):
         path = []
         c = True
         nodeInFocus = Graph.getVertex(endNodeIndex)
@@ -14,31 +13,34 @@ class BellmanFord():
         globalTraversalCost = 0
         for v in Graph.getVertexList():
             v.setBFParentExists(False)
+            v.setBFDistance(float("inf"))
         (Graph.getVertex(startNodeIndex)).setBFDistance(0)
         
         for itNum in Graph.getVertexList():
             for node in Graph.getVertexList():
                 if(node.getBFParentExists() == True or node.getID() == startNodeIndex):
                     for e in node.getOutgoingEdges():
-                        if((e.getToVertex()).getBFParentExists() == False and node.getID() != startNodeIndex):
+                        if((e.getToVertex()).getBFParentExists() == False) :
                             (e.getToVertex()).setBFParent(e)
-                            (e.getToVertex()).setBFDistance(node.getBFDistance()+e.getWeight())
-                            (e.getToVertex()).setBFParentExists(True)
+                            (e.getToVertex()).setBFDistance(node.getBFDistance() + e.getWeight())
+                            (e.getToVertex()).setBFParentExists(True) 
                         elif((e.getToVertex()).getBFDistance() > node.getBFDistance()+e.getWeight()):
                             (e.getToVertex()).setBFParent(e)
                             (e.getToVertex()).setBFDistance(node.getBFDistance()+e.getWeight())
         while(c == True):
+            nodeInFocus.highlight()
+            if nodeInFocus.getID() == startNodeIndex:
+                break
             edgeInFocus = nodeInFocus.getBFParent()
             nodeInFocus = edgeInFocus.getFromVertex()
-            nodeInFocus.highlight()
             path.append(edgeInFocus)
             globalTraversalCost = globalTraversalCost+1
-            if(globalTraversalCost >= len(Graph.getVertexList()):
+            if(globalTraversalCost >= len(Graph.getVertexList())):
                return False
             if(nodeInFocus.getID == startNodeIndex):
                 c = False
         path.reverse()
-        return path 
+        return path
         
                         
                     
